f50eb17f6abc704c47cf1bdb8312c1faa937d2e5
Sai Teja <saiteja13427@gmail.com>
2025-05-09 15:25:19 +0530

    Transformations

---
+++ transformations/src/main.cpp
@@ -0,0 +1,172 @@
+#include <iostream>
+#include <cmath>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "../libs/shader.h"
+#include "../libs/stb_image.h"
+
+float vertices[] = {
+    -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,    // Top Left
+    0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // Bottom right
+    -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, //Bottom left
+    0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f    // Top Right
+};
+
+int indices[] = {
+    0,1,3,
+    1,2,0
+};
+
+
+void framebuffer_size_callback (GLFWwindow* window, int width, int height){
+    glViewport(0,0,width, height);
+}
+
+void processInput (GLFWwindow *window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int main (){
+    // glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+    // glm::mat4 trans = glm::mat4(1.0f);
+    // trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+    // vec = trans * vec;
+    // std::cout << vec.x << " "  << vec.y << " " << vec.z << " " << vec.w << std::endl;
+
+    char infoLog[512];
+    int success, width, height, nRChannels;
+    /*
+     * VBO -> Vertex buffer object
+     * VAO -> Vertex array object
+     * EBO -> Element buffer object
+     */
+    unsigned int VBO, VAO, texture, texture1, EBO;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)){
+        std::cout << "Failed to init glad" << std::endl;
+        return -1;
+    }
+
+    glViewport(0,0,800,600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    int nAttribs;
+    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nAttribs);
+    std::cout << "Attribss: " << nAttribs << std::endl;
+
+
+    glGenBuffers(1, &EBO);
+    glGenVertexArrays(1, &VAO);
+    
+    /* Generate a buffer, bind it to GL_ARRAY_BUFFER type required by a VBO and copy the vertices into this buffer */
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);    
+    /* GL_(STATIC/DYNAMIC/STREAM))_DRAW call tells how many times will the data be set and how many times will it be used */
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    /* Tell OpenGL a few things about the vertex data */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6*sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    Shader sh1("/home/saiteja/Qualcomm/preparation/opengl/transformations/shaders/shader.vs", "/home/saiteja/Qualcomm/preparation/opengl/transformations/shaders/shader.frag");
+    
+    /* Need this otherwise, the images will be rendered vertically reversed. This is because opengl expects y 0 to be at bottom but images have it at top.*/
+    stbi_set_flip_vertically_on_load(true);
+
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    unsigned char* texData = stbi_load("/home/saiteja/Qualcomm/preparation/opengl/transformations/image/container.jpg", &width, &height, &nRChannels, 0);
+    
+    if(texData){
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, texData);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "ERROR::TEXTURE::LOAD_FAILED: " << std::endl;
+    }
+
+    stbi_image_free(texData);
+
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    unsigned char* smileData = stbi_load("/home/saiteja/Qualcomm/preparation/opengl/transformations/image/awesomeface.png", &width, &height, &nRChannels, 0);
+
+    if(smileData){
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, smileData);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+
+
+    /*
+     * Render Loop: Each iteration is called a frame
+     */
+    while (!glfwWindowShouldClose(window)){
+        /* Inputs */
+        processInput(window);
+
+        /* Render */
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        sh1.use();
+        sh1.setInt("inTexture2", 1);
+        unsigned int transformLoc = glGetUniformLocation(sh1.ID, "transform");
+        /* 
+        * Create a transformation matrix
+        */
+        glm::mat4 trans = glm::mat4(1.0f);
+        trans = glm::scale(trans, glm::vec3((float)glfwGetTime() / 10, 1.0f, 1.0f));
+        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(1.0f, 1.0f, 1.0f));
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        /* Poll events and swap buffers */
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+exit:
+    glfwTerminate();
+    return 0;
+}
